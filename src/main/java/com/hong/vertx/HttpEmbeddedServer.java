d8b02de6dacd8d8862f39a298c877991ddeac75e
allbegray <allbegray@nate.com>
2015-05-27 19:02:07 +0900

    샘플 hbs 템플릿 핸들러 적용

--- src/main/java/com/hong/vertx/HttpEmbeddedServer.java
+++ src/main/java/com/hong/vertx/HttpEmbeddedServer.java
@@ -10,9 +10,11 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.impl.LoggerFactory;
-import io.vertx.ext.apex.Route;
 import io.vertx.ext.apex.Router;
 import io.vertx.ext.apex.handler.BodyHandler;
+import io.vertx.ext.apex.handler.TemplateHandler;
+import io.vertx.ext.apex.templ.HandlebarsTemplateEngine;
+import io.vertx.ext.apex.templ.TemplateEngine;
 
 public class HttpEmbeddedServer {
 
@@ -32,7 +34,7 @@ public class HttpEmbeddedServer {
 				logger.info("헬로우 월드!");
 			}
 		};
-		
+
 		vertx.setPeriodic(60 * 1000, new Handler<Long>() {
 
 			@Override
@@ -40,30 +42,29 @@ public class HttpEmbeddedServer {
 				logger.info("60초마다 도는 이벤트!!");
 			}
 		});
-		
+
 		vertx.executeBlocking(new Handler<Future<String>>() {
 
 			@Override
 			public void handle(Future<String> event) {
-				
+
 				String result = "블락킹 된 서비스(예로 DB 에서 가져옴)에서 가져온 결과값";
 				event.complete(result);
-				
+
 			}
 		}, new Handler<AsyncResult<String>>() {
 
 			@Override
 			public void handle(AsyncResult<String> event) {
-				
+
 				event.result();
-				
+
 			}
 		});
-		
+
 		vertx.deployVerticle("com.hong.vertx.MyVerticle");
 		vertx.deployVerticle("com.hong.vertx.OtherVerticle");
-		
-		
+
 		EventBus eventBus = vertx.eventBus();
 		eventBus.consumer("com.hong.consumer", message -> {
 			logger.info("받은 메시지 : " + message.body());
@@ -75,11 +76,11 @@ public class HttpEmbeddedServer {
 				logger.info("답장 옴 올~ : " + event.result().body());
 			}
 		});
-		
+
 		Router router = Router.router(vertx);
-		
+
 		PersonJsonApiHandler personJsonApiHandler = new PersonJsonApiHandler();
-		
+
 		router.route().handler(BodyHandler.create());
 		router.get("/person/list").handler(personJsonApiHandler::handleGetPersons);
 		router.post("/person/add").handler(personJsonApiHandler::handleAddPerson);
@@ -92,6 +93,20 @@ public class HttpEmbeddedServer {
 			response.end("Hello World from Vert.x-Web!");
 
 		});
+
+		TemplateEngine engine = HandlebarsTemplateEngine.create();
+		TemplateHandler handler = TemplateHandler.create(engine);
+
+		router.get("/dynamic").handler(routingContext -> {
+
+			routingContext.put("request_path", routingContext.request().path());
+			routingContext.put("foo", "푸");
+
+			routingContext.next();
+			
+		});
+
+		router.get("/dynamic/").handler(handler);
 
 		httpServer.requestHandler(router::accept).listen(8080);
 
